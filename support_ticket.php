008aef3a22c3ae4359b3e6cc83bf97ff4e25df52
Mohammad Reza Nassari Asl <nasariasl@gmail.com>
2024-07-01 20:48:01 +0330

    Update support_ticket.php

--- support_ticket.php
+++ support_ticket.php
@@ -131,9 +131,31 @@ function support_ticket_output($vars) {
     $LANG = $vars['_lang'];
 
     echo '<p> صفحه مدیریت افزونه ثبت تیکت ' . date("Y-m-d H:i:s") . '</p>';
-
+	echo "modulelink = ".$modulelink;
         // Perform potentially risky queries in a transaction for easy rollback.
-        try {
+
+?>
+
+<ul class="nav nav-tabs" id="myTab" role="tablist">
+  <li class="nav-item">
+    <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Home</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Profile</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Contact</a>
+  </li>
+</ul>
+<div class="tab-content" id="myTabContent">
+  <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">...</div>
+  <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">...</div>
+  <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">...</div>
+</div>
+
+<?php	
+  if(isset($_POST["name"]) && !empty($_POST["name"]) ){ 
+	try {
             Capsule::connection()->transaction(
                 function ($connectionManager)
                 {
@@ -147,7 +169,7 @@ function support_ticket_output($vars) {
             );
         } catch (\Exception $e) {
             echo "Uh oh! Inserting didn't work, but I was able to rollback. {$e->getMessage()}";
-        }
+     }
 
 
 }
